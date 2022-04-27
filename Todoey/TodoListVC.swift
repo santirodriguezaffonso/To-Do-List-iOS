4a55c243b3acb31c10e3a23968daaccd885a7aa3
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-04-26 23:41:25 -0300

    Add numberOfRowsInSection and cellForRowAt methods

--- Todoey/TodoListVC.swift
+++ Todoey/TodoListVC.swift
@@ -3,12 +3,27 @@
 import UIKit
 
 class TodoListVC: UITableViewController {
+    
+    let itemArray = ["Mike", "Eleven", "Mark"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return
+//    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 }
+
 
