aa762e9d5f97ab0dec5f5ef159611c1bdb3838bf
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-06-01 20:09:49 -0300

    Add Unit Testing

---
+++ TodoeyTests/TodoeyTests.swift
@@ -0,0 +1,31 @@
+//
+//  TodoeyTests.swift
+//  TodoeyTests
+//
+//  Created by Santiago Rodriguez Affonso on 01/06/2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import XCTest
+
+// The next line is important in order to let you link with Todoey files, if you don't include test from the begenning.
+@testable import Todoey
+
+class TodoeyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+}
